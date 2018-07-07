e8eb1eb114d308513342c81737083278518e429f
tinny <tinnystudios@gmail.com>
2018-07-07 16:42:47 +1000

    Upgraded shooting

--- ProjectAwesome/Assets/_Project/Scripts/PlayerBullet.cs
+++ ProjectAwesome/Assets/_Project/Scripts/PlayerBullet.cs
@@ -8,24 +8,37 @@ public class PlayerBullet : MonoBehaviour
     public LayerMask hitMask;
 
     private float mSize = 0.5F;
+    private float mRange = 5;
+    private Vector3 mFiredPosition;
 
-    public void Fire(Vector3 dir, float force, float size)
+    public void Fire(Vector3 dir, float force, float size, float range)
     {
         m_RigidBody.velocity = dir * force;
         StartCoroutine(DestroyOverTime(5));
         transform.localScale = Vector3.one * size;
         mSize = size;
+        mRange = range;
+        mFiredPosition = transform.position;
     }
 
     void Update()
     {
+        if (Vector3.Distance(transform.position, mFiredPosition) >= mRange)
+        {
+            Destroy(gameObject);
+        }
+
         RaycastHit hit;
 
         if (Physics.SphereCast(transform.position, mSize, transform.forward, out hit, mSize, hitMask))
         {
-            var enemy = FindObjectOfType<EnemyStatus>();
-            enemy.TakeDamage(1);
-            Destroy(gameObject);
+            var enemy = hit.transform.GetComponent<EnemyStatus>();
+
+            if (enemy != null)
+            {
+                enemy.TakeDamage(1);
+                Destroy(gameObject);
+            }
         }
     }
 
--- ProjectAwesome/Assets/_Project/Scripts/RangeAttack.cs
+++ ProjectAwesome/Assets/_Project/Scripts/RangeAttack.cs
@@ -8,6 +8,15 @@ public class RangeAttack : MonoBehaviour, IRangeAttack, ICoolDownable
     private bool isActive = false;
     public float shootForce = 50;
     public float coolDownTime = 0.5F;
+    public float shootTime = 0.35F;
+
+    private float mStartTime = 0;
+    public AnimationCurve mLineCurve;
+    public float shootingRange = 20;
+    public float minShootingRange = 5;
+    private float mProgress;
+
+
     public float CoolDownTime
     {
         get
@@ -18,6 +27,19 @@ public class RangeAttack : MonoBehaviour, IRangeAttack, ICoolDownable
 
     public void Show(Vector2 joystickInput)
     {
+        if (!isActive)
+        {
+            mStartTime = Time.time;
+        }
+
+        var timeLapsed = Time.time - mStartTime;
+        var shootScale = m_ShootingLine.transform.localScale;
+        var t = timeLapsed / shootTime;
+        mProgress = t;
+        //10 meters
+        shootScale.z = Mathf.Lerp(minShootingRange, shootingRange, mLineCurve.Evaluate(t));
+        m_ShootingLine.transform.localScale = shootScale;
+
         isActive = true;
         m_ShootingLine.gameObject.SetActive(true);
 
@@ -27,7 +49,9 @@ public class RangeAttack : MonoBehaviour, IRangeAttack, ICoolDownable
         lookDirection.z = -joystickInput.y;
 
         transform.forward = new Vector3(-lookDirection.x, transform.forward.y, -lookDirection.z);
-        m_ShootingLine.forward = new Vector3(lookDirection.x, transform.forward.y, lookDirection.z);
+        m_ShootingLine.forward = transform.forward;
+
+
     }
 
     //joystick input = dir
@@ -38,7 +62,10 @@ public class RangeAttack : MonoBehaviour, IRangeAttack, ICoolDownable
         if (isActive)
         {
             var bullet = Instantiate(m_Bullet, transform.position, transform.rotation);
-            bullet.Fire(transform.forward, shootForce, 1.0F);
+
+            var range = Mathf.Lerp(minShootingRange,shootingRange,mProgress);
+
+            bullet.Fire(transform.forward, shootForce, 1.0F, range);
         }
 
         m_ShootingLine.gameObject.SetActive(false);
